d944b4b1d79498db857bde8b0cf9c52ca275e3d0
Arnaud Droit <arnaud.droit@ircm.qc.ca>
2009-10-27 18:31:23 +0000

    #label rtracklayer problem on Windows server 2008
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/rMAT@42677 bc3139a8-67e5-0310-9ffc-ced21a209358

--- scripts/test.R
+++ scripts/test.R
@@ -1,23 +1,24 @@
 # library(Biobase)
 library(rMAT)
 
-pwd<-"/rMAT/inst/doc/"
+pwd<-"/Volumes/GotGrid/Data/chip-chip/ER/"
 
 ####################################################
 #See vignette on how to get data files from Web site.
 ####################################################
 
-setwd("/rMAT/inst/doc/")  # Set wroking directory where you are your CEL and bpmap files
+setwd(" /Volumes/GotGrid/Data/chip-chip/ER/")  # Set wroking directory where you are your CEL and bpmap files
 
-bpmapFile<-paste(pwd,"Sc03b_MR_v04_10000.bpmap",sep="")
-arrayFile<-paste(pwd,c("Swr1WTIP_Short.CEL"),sep="")
+bpmapFile<-paste(pwd,"P1_CHIP_A.Anti-Sense.hs.NCBIv35.NR.bpmap",sep="")
+arrayFile<-paste(pwd,c("MCF_ER_A1.CEL","MCF_ER_A3.CEL","MCF_ER_A4.CEL","MCF_INP_A1.CEL","MCF_INP_A3.CEL","MCF_INP_A4.CEL"),sep="")
+
 
 # Show the all the different sequences
 ReadBPMAPAllSeqHeader(bpmapFile)
 
 # create a tiling Set from the corresponding data
 # This will only grep the sequences with Sc
-ScSet<-BPMAPCelParser(bpmapFile, arrayFile, verbose=TRUE,groupName="Sc")     
+ScSet<-BPMAPCelParser(bpmapFile, arrayFile, verbose=TRUE,groupName="",seqName="chr21")   
 
 # show the object
 show(ScSet)
@@ -39,9 +40,9 @@ summary(ScSetNorm)
 # So it's always a good idea to use unique identifiers for control and treatment
 #ScScore<-MATScore(ScSetNorm, cName=NULL, dMax=600, nProbesMin=8, dMerge=300, method="score", threshold=5, verbose=TRUE, bedName="MyBedFile")
 
-RD<-computeMATScore(ScSetNorm,cName=NULL, dMax=600, verbose=TRUE) 
+RD<-computeMATScore(ScSetNorm,cName="MCF_INP", dMax=600, verbose=TRUE) 
 
-Enrich<-callEnrichedRegions(RD,dMax=600, dMerge=300, nProbesMin=8, method="score", threshold=1, verbose=FALSE)  
+Enrich<-callEnrichedRegions(RD,dMax=600, dMerge=300, nProbesMin=8, method="pValue", threshold=1e-5, verbose=FALSE)  
 
 # Show the object
 show(ScScore)
@@ -78,26 +79,32 @@ view<- browserView(session,range(subEnrich) * -2)
 # 2) This is using the GenomeGraphs package
 ##########################################
 library(GenomeGraphs)
-mart<-useMart("ensembl", dataset = "scerevisiae_gene_ensembl")
+mart<-useMart("ensembl", dataset = "hsapiens_gene_ensembl")
 genomeAxis<-makeGenomeAxis(add53 = TRUE,add35 = TRUE)
 
 # Here I only look at chr1 between 1 and 50000
 
-minbase<-1 
-maxbase<-50000 
+minbase<-15400000
+maxbase<-15600000 
 
-genesplus<-makeGeneRegion(start = minbase, end = maxbase, strand = "+", chromosome = "I", biomart = mart) 
-genesmin<-makeGeneRegion(start = minbase, end = maxbase, strand = "-", chromosome = "I", biomart = mart)
+minbase<-14100000
+maxbase<-15000000
+
+genesplus<-makeGeneRegion(start = minbase, end = maxbase, strand = "+", chromosome = 21, biomart = mart) 
+genesmin<-makeGeneRegion(start = minbase, end = maxbase, strand = "-", chromosome = "21", biomart = mart)
 
 # Here I create a Generic array for chr1 ony
-RD1<-RD[space(RD)=="chr1",]
-Enrich1<-Enrich[space(Enrich)=="chr1",]
+RD1<-RD[space(RD)=="chr21",]
+Enrich1<-Enrich[space(Enrich)=="chr21",]
 MatScore<-makeGenericArray(intensity=as.matrix(score(RD1)),  probeStart=start(RD1), dp=DisplayPars(size=1, color="black", type="l"))
 
 # Here I create overlays to look at the enriched regions (above>threshold)
 rectList<- makeRectangleOverlay(start = start(Enrich1), end = end(Enrich1), region = c(1, 4), dp = DisplayPars(color = "green", alpha = 0.1))
 
-gdPlot(list("score" = MatScore, "Gene +" = genesplus, Position = genomeAxis, "Gene -" = genesmin), minBase = minbase, maxBase = maxbase, labelCex = 1, overlays=rectList) 
+gdPlot(list("score" = MatScore, "Gene +" = genesplus, Position = genomeAxis, "Gene -" = genesmin), minBase = minbase, maxBase = maxbase, labelCex = 1,overlays=rectList) 
+
+
+gdPlot(list("score" = MatScore,  Position = genomeAxis), minBase = minbase, maxBase = maxbase, labelCex = 1) 
 
 
 
