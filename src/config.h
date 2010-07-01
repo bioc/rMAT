defec38c1c69f7ffee645fb38af9141978fa84d8
Arnaud Droit <arnaud.droit@ircm.qc.ca>
2010-07-01 18:22:00 +0000

    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/rMAT@47802 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ src/config.h
@@ -0,0 +1,47 @@
+/* src/config.h.  Generated from config.h.in by configure.  */
+#ifndef CONFIG_H
+#define CONFIG_H
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "http://www.rglab.org"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "rMAT"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "rMAT 2.6.0"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "rmat"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "2.5.1"
+
+#ifdef HAVE_DISPATCH_DISPATCH_H
+  #include <dispatch/dispatch.h>
+  #define DO_NORMALIZE(task, counter_name, n_times) \
+    dispatch_apply(n_times, \
+                   dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0),	\
+                   ^(size_t counter_name) { task; });
+  
+#else // not HAVE_DISPATCH_DISPATCH_H
+  #ifdef HAVE_OMP_H
+    #include <omp.h>
+    #define DO_NORMALIZE(task, counter_name, n_times) \
+      _Pragma("omp parallel for")                     \
+      for (int counter_name = 0; counter_name < n_times; ++counter_name) { \
+        task; \
+      }
+  #else // not HAVE_OMP_H
+
+    #define DO_NORMALIZE(task, counter_name, n_times) \
+      for (int counter_name = 0; counter_name < n_times; ++counter_name) { \
+        task; \
+      }
+  #endif // HAVE_OMP_H
+#endif // HAVE_DISPATCH_DISPATCH_H
+
+#endif
